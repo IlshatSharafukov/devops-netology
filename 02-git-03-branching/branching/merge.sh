d4f389a13acaad778a0274ebe12f2d24fa22fed9
Ilshat <sharafukov@gmail.com>
2022-12-05 14:05:04 +0300

    merge: @ instead *

--- 02-git-03-branching/branching/merge.sh
+++ 02-git-03-branching/branching/merge.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# display command line options for me
+# display command line options
 
 count=1
-for param in "$*"; do
-    echo "\$* Parameter #$count = $param"
+for param in "$@"; do
+    echo "\$@ Parameter #$count = $param"
     count=$(( $count + 1 ))
 done
