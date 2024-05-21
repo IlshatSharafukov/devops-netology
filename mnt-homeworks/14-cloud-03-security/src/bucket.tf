c7d772423ea2dc9ac61b6e83967193dc73584d30
Ilshat Sharafukov <sharafukov@gmail.com>
2024-05-21 16:43:15 +0300

    выполнил дз 14-03

---
+++ mnt-homeworks/14-cloud-03-security/src/bucket.tf
@@ -0,0 +1,49 @@
+resource "yandex_storage_bucket" "ilshat" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket = "ilshat01"
+  acl    = "public-read"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "yandex_storage_object" "ilshat" {
+  bucket = "ilshat01"
+  key    = "cat.jpg"
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  source = "cat.jpg"
+}
+
+
+resource "yandex_iam_service_account" "sa" {
+  folder_id = var.folder_id
+  name      = "sa123"
+}
+
+
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = var.folder_id
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+
+resource "yandex_kms_symmetric_key" "key-a" {
+  name              = "net"
+  description       = "net"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h"
+}
